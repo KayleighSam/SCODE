46581b842a187549d4184332854a3192939fabb1
sam mwaura <samhanremson@gmail.com>
2025-05-14 20:35:43 +0300

    js work

---
+++ script.js
@@ -0,0 +1,16 @@
+//variables-data types  integers, boolean, string
+//arrays -  arrays methods- shift,unshift,pop,push iterations
+//functions
+//class - super class, inheritance
+//context
+//dome
+//use of let
+//use of const
+
+// typeof ()
+// console.log()
+//map
+// const name="sam"
+// const capitalName = 
+// console.log(capitalName)
+
