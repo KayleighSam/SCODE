332eb088be41f1f2b21312926d57c237bc404686
sam mwaura <samhanremson@gmail.com>
2025-05-15 10:33:08 +0300

    array methods

--- script.js
+++ script.js
@@ -14,3 +14,15 @@
 // const capitalName = 
 // console.log(capitalName)
 
+const fruits = ["banana", "apple", "mango"];
+const upperFruits = fruits.map(fruits => fruits.toUpperCase())
+upperFruits.sort()
+upperFruits.push("olive")
+console.log(upperFruits)
+// sorting fruits.sort()
+// removes the first item fruits.pop()
+// adds at the end fruits.push("avocado")
+// removes firts fruits.shift()
+//add at the first fruits.unshift("pinneaple")
+
+//map uppecase
